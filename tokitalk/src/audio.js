4772700e2465b372569121faa6b074d7209fc543
Ines HAMIDI <ineshamid@gmail.com>
2023-10-04 16:52:43 +0200

    la base du TTS, faudra mettre les variables

--- tokitalk/src/audio.js
+++ tokitalk/src/audio.js
@@ -2,4 +2,15 @@ export function lireText(text){
     if(text == "" || typeof text !== 'string'){
         return;
     }
+
+    var speech = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[10]; 
+    speech.volume = 1; // From 0 to 1
+    speech.rate = 1; // From 0.1 to 10
+    speech.pitch = 0; // From 0 to 2
+    speech.lang = 'fr';
+
+    speech.text = text;
+    window.speechSynthesis.speak(speech);
 }
