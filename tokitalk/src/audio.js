d3210b6a7c91f0740c3188cdbd3ee9380782644e
Ines HAMIDI <ineshamid@gmail.com>
2023-10-05 11:00:59 +0200

    audio js

--- tokitalk/src/audio.js
+++ tokitalk/src/audio.js
@@ -1,16 +1,26 @@
-export function lireText(text){
+export function initSpeech(){
+    var speech = new SpeechSynthesisUtterance();
+    //var voices = window.speechSynthesis.getVoices();
+    speech.voice = localStorage.getItem("voice"); 
+    speech.volume = localStorage.getItem("volume"); // From 0 to 1
+    speech.rate = localStorage.getItem("rate"); // From 0.1 to 10
+    speech.pitch = localStorage.getItem("pitch"); // From 0 to 2
+    speech.lang = localStorage.getItem("language");
+    return speech;
+}
+
+export function readText(text, speech){
     if(text == "" || typeof text !== 'string'){
         return;
     }
-
-    var speech = new SpeechSynthesisUtterance();
-    var voices = window.speechSynthesis.getVoices();
-    speech.voice = voices[10]; 
-    speech.volume = 1; // From 0 to 1
-    speech.rate = 1; // From 0.1 to 10
-    speech.pitch = 0; // From 0 to 2
-    speech.lang = 'fr';
-
     speech.text = text;
     window.speechSynthesis.speak(speech);
+}
+
+export function pause(speech){
+    speech.pause();
+}
+
+export function stop(speech){
+    speech.stop();
 }
--- tokitalk/src/components/TextAreaComponent.vue
+++ tokitalk/src/components/TextAreaComponent.vue
@@ -1,20 +1,28 @@
 <template>
     <div>
         <label for="TextArea">Entrez votre texte</label>
-        <textarea id="TextArea" maxlength="100" placeholder="Welcome ! Bienvenue !"></textarea>
-        <button id="b-valider"><font-awesome-icon :icon="['fas', 'check']" /></button>
+        <textarea v-model="text" id="TextArea" maxlength="100" placeholder="Welcome ! Bienvenue !"></textarea>
+        <button id="b-valider" @click="validateText()" :on-keypress="onPressEnter">
+            <font-awesome-icon :icon="['fas', 'check']" />
+        </button>
     </div>
 </template>
 <script>
+import readText from '@/audio';
+import initSpeech from '@/audio';
 
 export default {
     data(){
         return {
-            
+            text: String,
         }
     },
     methods: {
-        
+        validateText(){
+            var speech = initSpeech();
+            readText(this.text, speech);
+            this.text = "";
+        }
     }
 }
 </script>
