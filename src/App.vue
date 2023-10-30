a7a755c5b4b8a391ec24237678ca38932fd9ebe2
Ines HAMIDI <ineshamid@gmail.com>
2023-10-30 14:43:52 +0100

    On met le code qui lit le son dans le composant principal et on lira à partir de là

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,6 @@ import FooterComponent from './components/FooterComponent.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
 import SettingsLayerComponent from './components/SettingsLayerComponent.vue';
 import TextAreaComponent from './components/TextAreaComponent.vue';
-//import { inject } from 'vue';
 
 export default {
   name: 'App',
@@ -36,19 +35,51 @@ export default {
     FooterComponent
   },
 
-  setup() {
-    var buttons = ["bonjour", "oui", "non", "je me tire"];
-
-    localStorage.setItem("buttons", JSON.stringify(buttons) );
-    localStorage.setItem("pitch", 1);
-    localStorage.setItem("volume", 0.5);
-    localStorage.setItem("rate", 5);
-    localStorage.setItem("language", "fr-FR");
-  },
   data() {
     return {
+      speech: {},
     }
   },
+
+  mounted() {
+    this.initSpeech();
+  },
+
+  methods: {
+    initSpeech(){
+      this.speech =  new SpeechSynthesisUtterance();
+
+      this.speech.voice = window.speechSynthesis.getVoices()[1];
+
+      if(localStorage.getItem("volume") == null)
+        this.speech.volume = 1;
+      else
+        this.speech.volume = localStorage.getItem("volume"); // From 0 to 1
+
+      if(localStorage.getItem("rate") == null)
+        this.speech.rate = 5;
+      else
+        this.speech.rate = localStorage.getItem("rate"); // From 0.1 to 10
+
+      if(localStorage.getItem("pitch") == null)
+        this.speech.pitch = 1;
+      else
+        this.speech.pitch = localStorage.getItem("pitch"); // From 0 to 2
+
+      if(localStorage.getItem("language") == null)
+        this.speech.lang = "fr-FR"
+      else
+        this.speech.lang = localStorage.getItem("language");
+    },
+
+    readText(text){
+      if(text === "" || typeof(text) !== 'string'){
+        return;
+      }
+      this.speech.text = text;
+      window.speechSynthesis.speak(this.speech);
+    }
+  }
 }
 </script>
 
--- src/components/TextAreaComponent.vue
+++ src/components/TextAreaComponent.vue
@@ -1,52 +1,36 @@
 <template>
-    <div class="layer">
-        <div class="text-area">
-            <n-input type="textarea" size="small" :autosize="{
-                minRows: 3,
-                maxRows: 5
-            }" class="elements" v-model:value="text" maxlength="100" placeholder="Welcome ! Bienvenue !" />
-
-            <n-button type="primary" @click="validateText()"><i class="fa-solid fa-check"></i></n-button>
-
-            <!--:on-keypress="onPressEnter"-->
-        </div>
-    </div>
+  <div class="layer">
+    <n-form>
+      <n-form-item>
+        <n-input
+            class="text-area"
+            type="textarea"
+            placeholder="Hey !!"
+        />
+      </n-form-item>
+      <n-form-item>
+        <n-button @click="validateText">
+          <i class="fa-solid fa-check"></i>
+        </n-button>
+      </n-form-item>
+    </n-form>
+  </div>
 </template>
 <script>
-//import readText from '@/audio';
-//import { inject } from 'vue';
-
 export default {
-    setup() {
-        //      this.speech = inject('globalSpeech');
-    },
-    data() {
-        return {
-            text: "",
-            //speech: SpeechSynthesisUtterance,
-        }
-    },
-    methods: {
-        validateText() {
-            console.log(this.text)
-            //readText(this.text, this.speech);
-            this.text = "";
-        }
+  setup() {
+  },
+  data() {
+    return {
+      text: "",
     }
+  },
+  methods: {
+    validateText() {
+    }
+  }
 }
 </script>
 <style scoped>
 @import "../../public/cssVariables.css";
-/*
-.text-area{
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-}
-
-.elements{
-    margin-top: 20px;
-    margin-bottom: 20px;
-}
-*/
 </style>
