fd44063c3c14b24bf20a5295362aff72aaff7f00
Ines HAMIDI <ineshamid@gmail.com>
2023-10-31 11:00:07 +0100

    Faut revoir la conception pour la partie boutons

--- src/components/ButtonsLayerComponent.vue
+++ src/components/ButtonsLayerComponent.vue
@@ -1,54 +1,53 @@
 <template>
-    <div class="layer">
-        <div class="buttons-area">
-            <div class="perso-buttons">
-                <ButtonComponent v-for="button in this.buttons" :key="button"
-                    @click="readButton()">
-                    {{ button }}
-                </ButtonComponent>
-            </div>
+  <div class="layer">
+    <div class="buttons-area">
+      <div class="perso-buttons">
+        <button v-for="button in this.buttons" :key="button"
+                @click="$emit('button')">
+        </button>
+      </div>
 
-            <n-button type="primary" class="btn btn-primary" @click="createNewButton()">
-                <i class="fa-solid fa-plus"></i>
-            </n-button>
-        </div>
+      <n-button type="primary" class="btn btn-primary" @click='newClicked = !newClicked'>
+        <i class="fa-solid fa-plus"></i>
+      </n-button>
+      <n-input
+          :disabled='newClicked'
+          type="text"
+          size="small"
+      />
     </div>
+  </div>
 </template>
 <script>
-import ButtonComponent from "@/components/ButtonComponent.vue";
 export default {
-  components:{
-    ButtonComponent
+  setup() {
+    this.newClicked(false);
   },
-    setup() {
-    },
-    data() {
-        return {
-        }
-    },
-    methods: {
-        createNewButton(){
-
-        },
-
-        readButton(){
-
-        }
-    },
-    computed:{
-        buttons(){
-            return JSON.parse(localStorage.getItem("buttons"));
-        }
+  data() {
+    return {
+      newClicked: Boolean,
     }
+  },
+  methods: {
+    createNewButton() {
+
+    },
+
+  },
+  computed: {
+    buttons() {
+      return JSON.parse(localStorage.getItem("buttons"));
+    }
+  }
 }
 </script>
 <style scoped>
 @import "../../public/cssVariables.css";
 
 .buttons-area {
-    display: flex;
-    flex-direction: column;
-    overflow: scroll;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  align-items: center;
 }
 </style>
