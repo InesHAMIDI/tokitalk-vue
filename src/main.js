48cc796edf91b78716651ba885f7bce5843e8d6d
Ines HAMIDI <ineshamid@gmail.com>
2023-10-31 14:43:26 +0100

    Ajout de pinia pour gérer le stockage

--- src/main.js
+++ src/main.js
@@ -1,21 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import {
     // create naive ui
     create,
     // component
     NButton
   } from 'naive-ui'
-  
-  const naive = create({
-    components: [NButton]
-  })
 
- createApp(App)
-.use(naive)
+const naive = create({
+  components: [NButton]
+})
+
+const pinia = createPinia()
+
+createApp(App).use(createPinia())
+.use(naive).use(pinia)
 .mount('#app')
+
 //App.config.globalProperties.$globalSpeech = initSpeech();
 import "@fortawesome/fontawesome-free/css/all.css"
 
 //import { initSpeech } from './audio';
+
 
