3965cac34dcfa603455d0888871cda6034a2cfbf
Ines HAMIDI <ineshamid@gmail.com>
2023-12-28 17:13:59 +0100

    changes

---
+++ src/components/ButtonsLayerComponent.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="layer">
+    <div class="buttons-area">
+      <div class="perso-buttons">
+        <n-button v-for="button in this.buttons" :key="button"
+                  @click="$emit(button)">
+          {{ button }}
+        </n-button>
+      </div>
+
+      <n-button type="primary" class="btn btn-primary">
+        <i class="fa-solid fa-plus"></i>
+      </n-button>
+      <n-input
+          type="text"
+          size="tiny"
+      />
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { NInput} from 'naive-ui';
+export default {
+
+  components: {
+    NInput,
+  },
+
+  setup() {
+  },
+  data() {
+    return {
+      newClicked: Boolean,
+    }
+  },
+  methods: {
+
+  },
+  computed: {
+    buttons() {
+      return JSON.parse(localStorage.getItem("buttons"));
+    }
+  }
+}
+</script>
+<style scoped>
+@import "../../public/cssVariables.scss";
+
+.buttons-area {
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  align-items: center;
+}
+</style>
