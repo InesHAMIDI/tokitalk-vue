9e1de16e40b889db27a2a5bcbe0465fc716d79a9
Ines HAMIDI <ineshamid@gmail.com>
2024-01-05 15:35:31 +0100

    aled les langues

--- tokitalk-vite-project/src/App.vue
+++ tokitalk-vite-project/src/App.vue
@@ -1,16 +1,16 @@
 <script lang="ts">
 import FooterComponent from "./components/FooterComponent.vue";
 import ButtonsLayerComponent from "./components/ButtonsLayerComponent.vue";
-// import SettingsLayerComponent from "./components/SettingsLayerComponent.vue";
+import SettingsLayerComponent from "./components/SettingsLayerComponent.vue";
 import TextAreaLayerComponent from "./components/TextAreaLayerComponent.vue";
 
 export default {
   name: "App",
   components: {
+    SettingsLayerComponent,
     TextAreaLayerComponent,
     FooterComponent,
     ButtonsLayerComponent,
-    // SettingsLayerComponent,
   }
 }
 </script>
@@ -18,6 +18,7 @@ export default {
 <template>
   <ButtonsLayerComponent/>
   <TextAreaLayerComponent/>
+  <SettingsLayerComponent/>
   <FooterComponent/>
 
 </template>
--- tokitalk-vite-project/src/stores/settingsStore.ts
+++ tokitalk-vite-project/src/stores/settingsStore.ts
@@ -9,6 +9,64 @@ export const useSettingsStore = defineStore('setting', {
         volume: 0 //float between [0 ; 1]
     }),
     actions: {
+        setVoiceInd(index: number) {
+            this.voiceIndex = index;
+            localStorage.setItem("voiceIndex", this.voiceIndex.toString());
+        },
+        getVoiceIndex(): number {
+            var voiceInd = localStorage.getItem("voiceIndex");
+            if (voiceInd) {
+                this.voiceIndex = +voiceInd;
+            }
+            return this.voiceIndex;
+        },
 
+        setLangIndex(index: number) {
+            this.langIndex = index;
+            localStorage.setItem("langIndex", this.langIndex.toString());
+        },
+        getLangIndex(): number {
+            var langInd = localStorage.getItem("langIndex");
+            if (langInd) {
+                this.langIndex = +langInd;
+            }
+            return this.langIndex;
+        },
+
+        setRate(rate: number) {
+            this.rate = rate;
+            localStorage.setItem("rate", this.rate.toString());
+        },
+        getRate(): number {
+            var rate = localStorage.getItem("rate");
+            if (rate) {
+                this.rate = +rate;
+            }
+            return this.rate;
+        },
+
+        setPitch(pitch: number) {
+            this.pitch = pitch;
+            localStorage.setItem("pitch", this.pitch.toString());
+        },
+        getPitch(): number {
+            var pitch = localStorage.getItem("pitch");
+            if (pitch) {
+                this.pitch = +pitch;
+            }
+            return this.pitch;
+        },
+
+        setVolume(volume: number) {
+            this.volume = volume;
+            localStorage.setItem("volume", this.volume.toString());
+        },
+        getVolume(): number {
+            var volume = localStorage.getItem("volume");
+            if (volume) {
+                this.volume = +volume;
+            }
+            return this.volume;
+        }
     },
 });
