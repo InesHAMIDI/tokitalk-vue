ca5b6c0d409c3b2a00e3524e340e204722b7b29e
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2024-01-31 12:01:01 +0100

    on remet les dossiers correctement

--- components.d.ts
+++ components.d.ts
@@ -9,7 +9,6 @@ declare module 'vue' {
   export interface GlobalComponents {
     ButtonsLayerComponent: typeof import('./src/components/ButtonsLayerComponent.vue')['default']
     FooterComponent: typeof import('./src/components/FooterComponent.vue')['default']
-    HeaderComponent: typeof import('./src/components/HeaderComponent.vue')['default']
     KeybindComponent: typeof import('./src/components/KeybindComponent.vue')['default']
     NButton: typeof import('naive-ui')['NButton']
     NCarousel: typeof import('naive-ui')['NCarousel']
@@ -18,7 +17,6 @@ declare module 'vue' {
     NForm: typeof import('naive-ui')['NForm']
     NFormItem: typeof import('naive-ui')['NFormItem']
     NInput: typeof import('naive-ui')['NInput']
-    NModal: typeof import('naive-ui')['NModal']
     NSlider: typeof import('naive-ui')['NSlider']
     SettingsLayerComponent: typeof import('./src/components/SettingsLayerComponent.vue')['default']
     TextAreaLayerComponent: typeof import('./src/components/TextAreaLayerComponent.vue')['default']
