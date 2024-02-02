450001db1e21937a93cc3e96e9b92f4615f4fd83
Ines HAMIDI <ineshamid@gmail.com>
2024-02-02 14:42:37 +0100

    On vire pinia, nos classes font le taff

--- src/main.ts
+++ src/main.ts
@@ -8,14 +8,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {fas} from "@fortawesome/free-solid-svg-icons";
 import {far} from "@fortawesome/free-regular-svg-icons";
-import {pinia} from "./stores/store.ts";
-
 
 /* add all icons to the library */
 library.add(fas, far)
 
 const app = createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
-app.use(pinia)
-
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,6 @@ export default defineConfig({
                         'useLoadingBar'
                     ],
                 },
-                'pinia',
             ]
         }),
         Components({
